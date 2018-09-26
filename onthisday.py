ae533e7093527fff661d18a380f5295ff430866a
Hugo <hugovk@users.noreply.github.com>
2018-09-26 10:20:43 +0300

    Initial version

---
+++ onthisday.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+"""
+Tweet your old Flickr photos from this day in history.
+"""
+from __future__ import print_function
+import argparse
+import datetime
+import os
+import random
+import sys
+import yaml
+import webbrowser
+
+# http://www.stuvel.eu/flickrapi
+import flickrapi  # `pip install flickrapi`
+import flickrapi.shorturl
+# https://github.com/sixohsix/twitter
+# from twitter import *  # `pip install twitter`
+import twitter
+
+import flickr_utils
+
+
+def load_yaml(filename):
+    with open(filename) as f:
+        data = yaml.safe_load(f)
+
+    keys = data.viewkeys() if sys.version_info.major == 2 else data.keys()
+    if not keys >= {
+        'oauth_token', 'oauth_token_secret',
+        'consumer_key', 'consumer_secret'
+    }:
+        sys.exit("Twitter credentials missing from YAML: " + filename)
+    return data
+
+
+def tweet_it(string, credentials):
+    if len(string) <= 0:
+        return
+
+    # Create and authorise an app with (read and) write access at:
+    # https://dev.twitter.com/apps/new
+    # Store credentials in YAML file. See data/onthisday_example.yaml
+    t = twitter.Twitter(auth=twitter.OAuth(credentials['oauth_token'],
+                        credentials['oauth_token_secret'],
+                        credentials['consumer_key'],
+                        credentials['consumer_secret']))
+
+    print("TWEETING THIS:\n", string)
+
+    if args.test:
+        print("(Test mode, not actually tweeting)")
+    else:
+        result = t.statuses.update(status=string)
+        url = "http://twitter.com/" + \
+            result['user']['screen_name'] + "/status/" + result['id_str']
+        print("Tweeted:\n" + url)
+        if not args.no_web:
+            webbrowser.open(url, new=2)  # 2 = open in a new tab, if possible
+
+
+def six_months_ago(now):
+    import calendar
+    new_day = now.day
+    new_year = now.year
+
+    if now.month > 6:
+        new_month = now.month - 6
+    else:
+        new_month = now.month + 6
+        new_year = now.year - 1
+
+    days_in_new_month = calendar.monthrange(new_year, new_month)[1]
+    if new_day > days_in_new_month:
+        new_day = days_in_new_month
+
+    then = now.replace(year=new_year, month=new_month, day=new_day)
+    return then
+
+
+def six_months_from(now):
+    import calendar
+    new_day = now.day
+    new_year = now.year
+
+    if now.month > 6:
+        new_month = now.month - 6
+        new_year = now.year + 1
+    else:
+        new_month = now.month + 6
+
+    days_in_new_month = calendar.monthrange(new_year, new_month)[1]
+    if new_day > days_in_new_month:
+        # new_day = days_in_new_month
+        return None
+
+    then = now.replace(year=new_year, month=new_month, day=new_day)
+    return then
+
+
+def find_photos(flickr, my_nsid, tweet, now, earliest_year):
+    found = 0
+    # These look like "2012: http://flic.kr/p/bqhhhb":
+    tweetlets = []
+    for year in range(now.year-1, earliest_year-1, -1):
+        print("Checking", year)
+
+        photo = flickr_utils.most_interesting_today_in(
+            flickr, my_nsid, year, now=now)
+
+        if photo is not None:
+            print("Found a photo for", year)
+            found += 1
+#             ET.dump(photo)
+            photo_id = int(photo.attrib['id'])
+            url = flickrapi.shorturl.url(photo_id)
+            tweetlet = str(year) + ": " + url
+            tweetlets.append(tweetlet)
+        else:
+            print("No photo for", year)
+
+    print("Found", found, "photos")
+
+    # There's room for four tweetlets in a tweet
+    if len(tweetlets) > 4:
+        tweetlets = random.sample(tweetlets, 4)
+
+    # In random order
+    # tweet += " " + " ".join(sorted(tweetlets, reverse=True))
+    tweet += " " + " ".join(tweetlets)
+
+    return found, tweet
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Tweet your old Flickr photos on this day in history.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-u', '--username', default="hugovk",
+        help="Your Twitter username")
+    parser.add_argument(
+        '-y', '--yaml',
+        default='/Users/hugo/Dropbox/bin/data/onthisday.yaml',
+        help="YAML file location containing Twitter keys and secrets")
+    parser.add_argument(
+        '-e', '--earliest-year', default=2004, type=int,
+        help="Earliest year to check for photos. "
+        "If 'None', uses the year of your oldest uploaded photo.")
+    parser.add_argument(
+        '-6', '--six-months', action='store_true',
+        help="Show photos from six months ago instead of on this day")
+    parser.add_argument(
+        '-x', '--test', action='store_true',
+        help="Test mode: go through the motions but don't add any photos")
+    parser.add_argument(
+        '-k', '--api-key',
+        help="Flickr API key. "
+        "If not given, looks in FLICKR_API_KEY environment variable")
+    parser.add_argument(
+        '-s', '--api-secret',
+        help="Flickr API secret. "
+        "If not given, looks in FLICKR_SECRET environment variable")
+    parser.add_argument(
+        '-nw', '--no-web', action='store_true',
+        help="Don't open a web browser to show the tweeted tweet")
+    args = parser.parse_args()
+
+    try:
+        import timing  # optional
+        assert timing  # silence warnings
+    except ImportError:
+        pass
+
+    twitter_credentials = load_yaml(args.yaml)
+
+    if not args.api_key:
+        args.api_key = os.environ['FLICKR_API_KEY']
+    if not args.api_secret:
+        args.api_secret = os.environ['FLICKR_SECRET']
+    flickr = flickrapi.FlickrAPI(args.api_key, args.api_secret)
+    flickr.authenticate_via_browser(perms='write')
+
+    if args.test:
+        print("(Test mode, not actually tweeting)")
+
+    my_nsid = flickr.people_findByUsername(username=args.username)
+    my_nsid = my_nsid.getchildren()[0].attrib['nsid']
+    print("My NSID:", my_nsid)
+
+    if args.earliest_year:
+        earliest_year = args.earliest_year
+    else:
+        person_info = flickr.people_getInfo(user_id=my_nsid)
+        firstdatetaken = person_info.getchildren()[0].find(
+            'photos').find('firstdatetaken').text
+
+        # User may have posted (for example, like me) an 19th century photo,
+        # but it doesn't matter, this is just an upper limit which may not be
+        # reached before the max tweet length is reached.
+        earliest_year = int(firstdatetaken[:4])
+
+    print("Earliest year:", earliest_year)
+
+    now = datetime.datetime.now()
+    found = 0
+
+    if not args.six_months:
+        tweet = "#OnThisDay"
+        found, tweet = find_photos(flickr, my_nsid, tweet, now, earliest_year)
+
+    if not found or args.six_months:
+        tweet = "#6MonthsAgo"
+        now = six_months_from(now)
+        if now:
+            found, tweet = find_photos(
+                flickr, my_nsid, tweet, now, earliest_year)
+
+    if not found:
+        sys.exit("No photos found, try again tomorrow")
+
+    print("Tweet this:\n", tweet)
+    tweet_it(tweet, twitter_credentials)
+
+# End of file
---
+++ test_onthisday.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+"""
+Unit tests for contact_sheet.py
+"""
+import datetime
+import unittest
+import onthisday
+
+class TestSequenceFunctions(unittest.TestCase):
+
+#     def setUp(self):
+#         self.seq = range(10)
+
+    def daterange(self, start_date, end_date):
+        """ http://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python """
+        for n in range(int ((end_date - start_date).days)):
+            yield start_date + datetime.timedelta(n)
+
+    def test_changed(self):
+        """ Test return isn't same as parameter """
+        input = datetime.datetime.now()
+        output = onthisday.six_months_ago(input)
+        self.assertNotEqual(input, output)
+
+    def test_dec_to_june(self):
+        """ Test 22 December 2013 -> 22 June 2013 """
+        input = datetime.datetime(2013, 12, 22)
+        expected_output = datetime.datetime(2013, 06, 22)
+        output = onthisday.six_months_ago(input)
+        self.assertEqual(expected_output, output)
+
+    def test_jan_to_july(self):
+        """ Test 20 January 2014 -> 20 July 2013 """
+        input = datetime.datetime(2014, 1, 20)
+        expected_output = datetime.datetime(2013, 7, 20)
+        output = onthisday.six_months_ago(input)
+        self.assertEqual(expected_output, output)
+
+    def test_from_31st_jan(self):
+        """ Test 31 January 2014 -> 31 July 2013 """
+        input = datetime.datetime(2014, 1, 31)
+        expected_output = datetime.datetime(2013, 7, 31)
+        output = onthisday.six_months_ago(input)
+        self.assertEqual(expected_output, output)
+
+    def test_from_31st_march(self):
+        """ Test 31 March 2014 -> 30 September 2013 """
+        input = datetime.datetime(2014, 3, 31)
+        expected_output = datetime.datetime(2013, 9, 30)
+        output = onthisday.six_months_ago(input)
+        self.assertEqual(expected_output, output)
+
+    def test_to_last_of_feb(self):
+        """ Test 31 August 2014 -> 28 Feb 2014 """
+        input = datetime.datetime(2014, 8, 31)
+        expected_output = datetime.datetime(2014, 2, 28)
+        output = onthisday.six_months_ago(input)
+        self.assertEqual(expected_output, output)
+
+    def test_to_leap_day(self):
+        """ Test 31 August 2012 -> 29 Feb 2012 """
+        input = datetime.datetime(2012, 8, 31)
+        expected_output = datetime.datetime(2012, 2, 29)
+        output = onthisday.six_months_ago(input)
+        self.assertEqual(expected_output, output)
+
+    def test_lots_of_days(self):
+        """ Tests:
+            No "ValueError: day is out of range for month" exceptions raised (e.g. for 30 Feb),
+            output value is different from input, and
+            output value is earlier than input
+        """
+        start_date = datetime.datetime(2000, 1, 1)
+        end_date = datetime.datetime(2030, 12, 31)
+        for input in self.daterange(start_date, end_date):
+            output = onthisday.six_months_ago(input)
+            self.assertNotEqual(input, output)
+            self.assertTrue(output < input)
+
+    def test_2_changed(self):
+        """ Test return isn't same as parameter """
+        input = datetime.datetime.now()
+        output = onthisday.six_months_from(input)
+        self.assertNotEqual(input, output)
+
+    def test_2_dec_to_june(self):
+        """ Test 22 December 2013 -> 22 June 2014 """
+        input = datetime.datetime(2013, 12, 22)
+        expected_output = datetime.datetime(2014, 06, 22)
+        output = onthisday.six_months_from(input)
+        self.assertEqual(expected_output, output)
+
+    def test_2_jan_to_july(self):
+        """ Test 20 January 2014 -> 20 July 2014 """
+        input = datetime.datetime(2014, 1, 20)
+        expected_output = datetime.datetime(2014, 7, 20)
+        output = onthisday.six_months_from(input)
+        self.assertEqual(expected_output, output)
+
+    def test_2_from_31st_jan(self):
+        """ Test 31 January 2014 -> 31 July 2014 """
+        input = datetime.datetime(2014, 1, 31)
+        expected_output = datetime.datetime(2014, 7, 31)
+        output = onthisday.six_months_from(input)
+        self.assertEqual(expected_output, output)
+
+    def test_2_from_31st_march(self):
+        """ Test 31 March 2014 -> None """
+        input = datetime.datetime(2014, 3, 31)
+        expected_output = None
+        output = onthisday.six_months_from(input)
+        self.assertEqual(expected_output, output)
+
+    def test_2_to_last_of_feb(self):
+        """ Test 31 August 2014 -> 28 Feb 2015 """
+        input = datetime.datetime(2014, 8, 31)
+        expected_output = None
+        output = onthisday.six_months_from(input)
+        self.assertEqual(expected_output, output)
+
+    def test_2_to_leap_day(self):
+        """ Test 31 August 2011 -> 29 Feb 2012 """
+        input = datetime.datetime(2011, 8, 31)
+        expected_output = None
+        output = onthisday.six_months_from(input)
+        self.assertEqual(expected_output, output)
+
+    def test_2_lots_of_days(self):
+        """ Some dates with day > 28 will return None. Forget those.
+            Tests:
+            days 1-28 don't return None
+            output value is different from input, and
+            output value is later than input
+        """
+        for year in range(2000, 2030+1):
+            for month in range(1, 12):
+                for day in range(1, 28):
+                    input = datetime.datetime(year, month, day)
+                    output = onthisday.six_months_from(input)
+                    self.assertNotEqual(None, output)
+                    self.assertNotEqual(input, output)
+                    self.assertTrue(output > input)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
